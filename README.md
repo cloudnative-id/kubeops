d14e7197f12943cbf665b6ecd1494ef0b01c4db3
Giri Kuncoro <girikuncoro@gmail.com>
2019-12-31 15:57:42 +0700

    Init argocd deployment for GitOps CD

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Kubernetes Operations (kubeops)
 "GitOps" repo for Cloud Native Indonesia Kubernetes environment. This repo controls the applications running in the kubernetes environment available to the Cloud Native Indonesia community. 
+
+## Quickstart
+
+Argocd is used for continuous delivery and GitOps sync of this repo
+(WIP), modify bootstrap.sh with your account and execute it:
+```sh
+./scripts/bootstrap.sh
+```
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+err_report() {
+  echo "Exited with error on line $1"
+}
+trap 'err_report $LINENO' ERR
+
+IFS=$'\n\t'
+
+function grant_cluster_admin {
+  local username="$1"
+  kubectl create clusterrolebinding $username-cluster-admin-binding --clusterrole=cluster-admin --user=$username@gmail.com
+}
+
+function init_argocd {
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+  # TODO(giri): Should be exposed through ingress controller, doing this temporarily
+  kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+}
+
+function download_argocd_tools() {
+  if [ "$(uname)" == "Darwin" ]; then
+    brew tap argoproj/tap
+    brew install argoproj/tap/argocd
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+    curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+    chmod +x /usr/local/bin/argocd
+  fi
+}
+
+### main program
+init_argocd
+download_argocd_tools
+
+# TODO(admin): Anyone that wants to execute argocd need to be cluster admin
+grant_cluster_admin girikuncoro
